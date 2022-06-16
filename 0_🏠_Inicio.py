99bb5c1a04afa44d179ec86b4f2bb6d5af88085d
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-16 13:12:16 +0200

    Update 0_🏠_Inicio.py

--- 0_🏠_Inicio.py
+++ 0_🏠_Inicio.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.write('Hola')
-st.dataframe(pd.read_csv('datos.csv',sep =';',encoding='latin-1'))
+
+df_datos = pd.read_csv('datos.csv',sep =';',encoding='latin-1')
+st.dataframe(df_datos)
+st.write(df_datos.enunciados)
