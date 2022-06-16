162db28654ffafe9cfa805cbab4bde907dadba8f
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-16 13:19:51 +0200

    Update 0_🏠_Inicio.py

--- 0_🏠_Inicio.py
+++ 0_🏠_Inicio.py
@@ -6,3 +6,9 @@ st.write('Hola')
 df_datos = pd.read_csv('datos.csv',sep =';',encoding='latin-1')
 st.dataframe(df_datos)
 st.write(df_datos.enunciado)
+
+option = st.selectbox(
+     'Bloque',
+     df_datos.enunciado)
+
+st.write('Has seleccionado:', option)
