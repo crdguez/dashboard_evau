086f2fa6fa87d61644fa9c161b2bf31fc879c8f9
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-16 17:20:08 +0200

    Update 0_🏠_Inicio.py

--- 0_🏠_Inicio.py
+++ 0_🏠_Inicio.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 st.write('Hola')
 
-#df_datos = pd.read_csv('datos.csv',sep =';',encoding='latin-1')
-df_datos = pd.read_csv('datos.csv',sep =';')
+df_datos = pd.read_csv('datos.csv',sep =';',encoding='latin-1')
+#df_datos = pd.read_csv('datos.csv',sep =';')
 
 st.dataframe(df_datos)
 st.write(df_datos.enunciado)
