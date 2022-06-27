a0f4727587b709d5e8598c964e9f152cde90bd73
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-27 10:49:48 +0200

    Update 0_🏠_Inicio.py

--- 0_🏠_Inicio.py
+++ 0_🏠_Inicio.py
@@ -24,24 +24,32 @@ st.write(df_datos.tags.str.split(',', expand=True))
 #pd.concat([dat1, dat2], axis=1)
 df_tags=df_datos.tags.str.split(',', expand=True)
 st.write(list(pd.concat([df_tags[0], df_tags[1]], axis=0).unique()))
-#pd.DataFrame(list(df_datos.tags.split(','))).set_index(0)[1]
-code ="""
-<iframe
-  src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1"
-  width="100%"
-  height="500px"
->
-</iframe>
-"""
-code ="""
-<iframe
-  src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1&code=from sympy import *
- "
-  width="100%"
-  height="500px"
->
-</iframe>
-"""
 
-components.html(code)
+pd.DataFrame(list(df_datos.tags.split(','))).set_index(0)[1]
+
+# lista de los diferentes tags
+
+st.write(list(set([x for xs in df_datos.tags.apply(lambda x: x.split(',')) for x in xs])))
+
+# Incluir una terminal de python para hacer pruebas
+
+# code ="""
+# <iframe
+#   src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1"
+#   width="100%"
+#   height="500px"
+# >
+# </iframe>
+# """
+# code ="""
+# <iframe
+#   src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1&code=from sympy import *
+#  "
+#   width="100%"
+#   height="500px"
+# >
+# </iframe>
+# """
+
+# components.html(code)
 
