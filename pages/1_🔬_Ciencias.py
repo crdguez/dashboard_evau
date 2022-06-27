cec2a9883538b976dd3217e03a56cd382a86153b
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-27 12:45:22 +0200

    Update 1_🔬_Ciencias.py

--- pages/1_🔬_Ciencias.py
+++ pages/1_🔬_Ciencias.py
@@ -6,13 +6,11 @@ st.title('EVAU Ciencias')
 df_datos = pd.read_csv('datos.csv',sep =';')
 
 st.write(df_datos.enunciado)
-# for ej in df_datos :
-#   st.write(str(ej.enunciado))
 
 for ej in df_datos.iterrows() :
     st.header(ej[1].bloque)
-    st.subheader(ej[1].anyo)
-#     st.image('img'+ej[1].enunciado)
+    st.subheader(ej[1].anyo + '-' + ej[1].convocatoria)
+    st.image('img/'+ej[1].enunciado)
 
 
-st.image('img/caj2101.png')
+# st.image('img/caj2101.png')
