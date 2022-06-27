96857f1a8aa156cc44f332424cfec626d194f891
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-27 12:30:36 +0200

    Update 1_🔬_Ciencias.py

--- pages/1_🔬_Ciencias.py
+++ pages/1_🔬_Ciencias.py
@@ -6,5 +6,8 @@ st.title('EVAU Ciencias')
 df_datos = pd.read_csv('datos.csv',sep =';')
 
 st.write(df_datos.enunciado)
-for ej in df_datos :
-  st.write(str(ej.enunciado))
+# for ej in df_datos :
+#   st.write(str(ej.enunciado))
+
+for ej in df_datos.iterrows() :
+    st.write(ej)
