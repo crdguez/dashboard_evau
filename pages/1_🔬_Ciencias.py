189edaa898605efcd671ea15ff6fa6e00d74318e
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-27 12:20:13 +0200

    Update 1_🔬_Ciencias.py

--- pages/1_🔬_Ciencias.py
+++ pages/1_🔬_Ciencias.py
@@ -6,5 +6,5 @@ st.title('EVAU Ciencias')
 df_datos = pd.read_csv('datos.csv',sep =';')
 
 st.write(df_datos.enunciado)
-for im in df_datos.enunciado :
+for im in df_datos :
   st.image(df_datos.enunciado)
