41033e372b9f3924c639231eea56c3ad6d5ab4ca
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-27 13:05:36 +0200

    Update 1_🔬_Ciencias.py

--- pages/1_🔬_Ciencias.py
+++ pages/1_🔬_Ciencias.py
@@ -8,8 +8,8 @@ df_datos = pd.read_csv('datos.csv',sep =';')
 # st.write(df_datos.enunciado)
 
 for ej in df_datos.iterrows() :
-    st.header(ej[1].bloque.capitalize()+str(ej[1].convocatoria).capitalize() + '-' + str(ej[1].anyo))
-    st.subheader(str(ej[1].anyo) + '-' + str(ej[1].convocatoria).capitalize())
+    st.subheader(ej[1].bloque.capitalize()+'-'+str(ej[1].convocatoria).capitalize() + '-' + str(ej[1].anyo))
+#     st.subheader(str(ej[1].anyo) + '-' + str(ej[1].convocatoria).capitalize())
 #     st.subheader(ej[1].anyo)
     st.image('img/'+ej[1].enunciado)
     with st.expander("Ver solución"):
