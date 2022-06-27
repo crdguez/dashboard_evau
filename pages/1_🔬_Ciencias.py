c60a1bf9e4e3d5d086bde72b49d533a3b199234c
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-27 13:04:12 +0200

    Update 1_🔬_Ciencias.py

--- pages/1_🔬_Ciencias.py
+++ pages/1_🔬_Ciencias.py
@@ -8,7 +8,7 @@ df_datos = pd.read_csv('datos.csv',sep =';')
 # st.write(df_datos.enunciado)
 
 for ej in df_datos.iterrows() :
-    st.header(ej[1].bloque.capitalize())
+    st.header(ej[1].bloque.capitalize()+str(ej[1].convocatoria).capitalize() + '-' + str(ej[1].anyo))
     st.subheader(str(ej[1].anyo) + '-' + str(ej[1].convocatoria).capitalize())
 #     st.subheader(ej[1].anyo)
     st.image('img/'+ej[1].enunciado)
