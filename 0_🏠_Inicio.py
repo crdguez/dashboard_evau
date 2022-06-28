2a24f9003827e14ffc135fd72352c30493770812
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-28 11:22:40 +0200

    Update 0_🏠_Inicio.py

--- 0_🏠_Inicio.py
+++ 0_🏠_Inicio.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 st.title('Ejercicios de Matemáticas de la EVAU ')
-st.write(':left: A la izquierda tienes un filtro')
+st.write(':arrow_left: A la izquierda tienes el menú')
 
 #df_datos = pd.read_csv('datos.csv',sep =';',encoding='latin-1')
 df_datos = pd.read_csv('datos.csv',sep =';')
