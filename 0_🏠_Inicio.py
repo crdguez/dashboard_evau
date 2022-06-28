e8a941d9438623a41f27c24735921c4e34f838e8
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-06-28 12:00:48 +0200

    Update 0_🏠_Inicio.py

--- 0_🏠_Inicio.py
+++ 0_🏠_Inicio.py
@@ -3,7 +3,7 @@ import pandas as pd
 # import streamlit.components.v1 as components
 
 
-st.title('Ejercicios de Matemáticas de la EVAU ')
+st.title('Exámenes de Matemáticas - EVAU ')
 st.write(':arrow_left: A la izquierda tienes el menú')
 
 st.info('Esta aplicación no es más que un filtro de los ejercicios que aparecen en [https://matematicasentumundo.es/PAU/PAU.htm](https://matematicasentumundo.es/PAU/PAU.htm)')
