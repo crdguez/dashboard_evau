9d14744083270af6a84fdefd3bdab0b927446ca9
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-07-04 09:42:14 +0200

    Update 0_🏠_Inicio.py

--- 0_🏠_Inicio.py
+++ 0_🏠_Inicio.py
@@ -5,7 +5,7 @@ import pandas as pd
 #st.set_page_config(page_title='Matemáticas EVAU', page_icon=':page_facing_up:', layout="wide", initial_sidebar_state="expanded", menu_items=None)
 st.set_page_config(
     page_title='Matemáticas EVAU',
-    page_icon=":shark:",
+    page_icon=":page_facing_up:",
 #     layout='wide'
     )
 
